0e1240d17e046e9faf1c0f18381e38a54a9251b6
Amir Asaad <amir.pro.2010@gmail.com>
2026-03-05 19:19:27 +0200

    :sparkles: feat: add Wallet, Lending, and Risk services

--- MercuryPay.AppHost/AppHost.cs
+++ MercuryPay.AppHost/AppHost.cs
@@ -1,12 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.MercuryPay_PaymentService>("paymentservice")
+var paymentService = builder.AddProject<Projects.MercuryPay_PaymentService>("paymentservice")
+    .WithHttpHealthCheck("/health");
+
+var walletService = builder.AddProject<Projects.MercuryPay_WalletService>("walletservice")
+    .WithHttpHealthCheck("/health");
+
+var lendingService = builder.AddProject<Projects.MercuryPay_LendingService>("lendingservice")
+    .WithHttpHealthCheck("/health");
+
+var riskService = builder.AddProject<Projects.MercuryPay_RiskService>("riskservice")
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.MercuryPay_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
+    .WithReference(paymentService)
+    .WaitFor(paymentService);
 
 builder.Build().Run();
